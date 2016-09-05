6302197b3005b6a390407cebf63df1c70833065a
IJBardales <olmccb@hotmail.com>
2016-09-05 19:24:36 -0400

    This commit contains code that controls the hadouken animating across the screen

--- js/app.js
+++ js/app.js
@@ -12,9 +12,16 @@ $(document).ready(function() {
   	// play hadouken sound
   	$('.ryu-ready').hide();
   	$('.ryu-throwing').show();
-  	$('.hadouken').show();
-  	// show hadouken and animate it to the right of the screen
-  })
+  	$('.hadouken').show().animate(
+  		{'left': '1020px'},
+  		500,
+  		function() {
+  			$('.hadouken').hide();
+  			$('.hadouken').css('left', '609px');
+  		});
+
+  	})
+  	
   .mouseup(function(){
   	//ryu goes back to his ready position
   	$('.ryu-throwing').hide();
